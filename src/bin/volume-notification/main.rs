78841b54564484e50cafe358de4e5760bae3cd59
jakobrs <jakobrs100@gmail.com>
2022-04-10 19:06:50 +0200

    switch to tokio

--- src/bin/volume-notification/main.rs
+++ src/bin/volume-notification/main.rs
@@ -3,9 +3,13 @@ mod notify;
 use std::{cell::RefCell, collections::HashMap, path::PathBuf, rc::Rc};
 
 use anyhow::Result;
-use async_std::{os::unix::net::UnixDatagram, prelude::StreamExt};
 use clap::Parser;
+use futures_util::StreamExt;
 use serde::Deserialize;
+use tokio::{
+    net::UnixDatagram,
+    task::{JoinHandle, LocalSet},
+};
 use zbus::{zvariant::Value, Connection};
 
 use notify::Notification;
@@ -28,60 +32,70 @@ struct Opts {
 
 const MAX_LENGTH: usize = 1024;
 
-#[async_std::main]
+#[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
     env_logger::init();
     let opts = Opts::parse();
 
-    if opts.socket.exists() {
-        std::fs::remove_file(&opts.socket)?;
-    }
-    let socket = UnixDatagram::bind(opts.socket).await?;
-
-    let mut buf = [0u8; MAX_LENGTH];
-    let tags: Rc<RefCell<HashMap<String, u32>>> = Rc::new(RefCell::new(HashMap::new()));
-
+    let ids = Rc::new(RefCell::new(HashMap::new()));
     let connection = Rc::new(Connection::session().await?);
 
-    {
+    let local_set = LocalSet::new();
+
+    local_set.spawn_local({
         let connection = connection.clone();
-        let tags = tags.clone();
-        async_std::task::spawn_local(async move {
+        let ids = ids.clone();
+
+        async move {
             let mut stream = notify::closed(&connection).await.unwrap();
 
             while let Some(msg) = stream.next().await {
                 let args = msg.args().unwrap();
 
-                tags.borrow_mut().retain(|_k, v| v != args.id())
-            }
-        });
-    }
+                log::debug!("Removing ids");
 
-    loop {
-        let count = socket.recv(&mut buf).await?;
-
-        log::debug!("Received message: {:?}", std::str::from_utf8(&buf[..count]));
-
-        match serde_json::from_slice(&buf[..count]) {
-            Ok(NotificationRequest { tag, body, value }) => {
-                let mut notification = Notification::new();
-                notification.summary(&tag);
-                notification.timeout(opts.duration);
-                if let Some(body) = body {
-                    notification.body(&body);
-                }
-                if let Some(value) = value {
-                    notification.add_hint("value", Value::I32(value));
-                }
-
-                // cannot use the entry API because of the await point
-                let &old_id = tags.borrow().get(&tag).unwrap_or(&0);
-                let id = notification.replaces_id(old_id).show(&connection).await?;
-                tags.borrow_mut().insert(tag, id);
-            }
-            Err(err) => {
-                log::error!("Error: {err:?}");
+                ids.borrow_mut().retain(|_k, v| v != args.id())
             }
         }
-    }
+    });
+
+    let main_loop: JoinHandle<Result<()>> = local_set.spawn_local(async move {
+        if opts.socket.exists() {
+            std::fs::remove_file(&opts.socket)?;
+        }
+        let socket = UnixDatagram::bind(opts.socket)?;
+
+        let mut buf = [0u8; MAX_LENGTH];
+        loop {
+            let count = socket.recv(&mut buf).await?;
+
+            log::debug!("Received message: {:?}", std::str::from_utf8(&buf[..count]));
+
+            match serde_json::from_slice(&buf[..count]) {
+                Ok(NotificationRequest { tag, body, value }) => {
+                    let mut notification = Notification::new();
+                    notification.summary(&tag);
+                    notification.timeout(opts.duration);
+                    if let Some(body) = body {
+                        notification.body(&body);
+                    }
+                    if let Some(value) = value {
+                        notification.add_hint("value", Value::I32(value));
+                    }
+
+                    // cannot use the entry API because of the await point
+                    let &old_id = ids.borrow().get(&tag).unwrap_or(&0);
+                    let id = notification.replaces_id(old_id).show(&connection).await?;
+                    ids.borrow_mut().insert(tag, id);
+                }
+                Err(err) => {
+                    log::error!("Error: {err:?}");
+                }
+            }
+        }
+    });
+
+    local_set.run_until(main_loop).await??;
+
+    Ok(())
 }
